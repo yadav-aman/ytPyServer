69a6943c087e44f7836eb8b3634f14aa8a457912
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2021-12-22 13:31:20 +0530

    response sorted in descending order of published datetime

--- server/routes/data.py
+++ server/routes/data.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi_pagination import Page, add_pagination, paginate
+from sqlalchemy import desc
 
 from database.main import get_db
 from schemas import videos as videoSchema
@@ -11,7 +12,8 @@ router = APIRouter(prefix='/data', tags=["Data"])
 
 @router.get('/', response_model=Page[videoSchema.Show_Video])
 async def get_data(db: Session = Depends(get_db)):
-    data = db.query(models.Video).all()
+    data = db.query(models.Video).order_by(
+        desc(models.Video.published_at)).all()
     return paginate(data)
 
 add_pagination(router)
